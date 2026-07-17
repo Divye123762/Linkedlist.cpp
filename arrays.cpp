bfa5179d8cf4d69c61d51e6fe2c6da3e317a27d6
Divye Khanna <divyekhanna852@gmail.com>
2026-07-17 22:20:27 +0530

    Add arrays.cpp

---
+++ arrays.cpp
@@ -0,0 +1,178 @@
+#include<iostream>
+#include<climits>
+ using namespace std;
+
+//  void change(int x[]){
+//     x[0]=9;
+//  }
+
+int main(){
+
+    // int arr[]={10,20,30,40,50};
+    // cout<<arr[0]<<endl;
+    // cout<<arr[1]<<endl;
+    // cout<<arr[2]<<endl;
+    // cout<<arr[3]<<endl;
+    // cout<<arr[4]<<endl;
+
+    // for(int i=0;i<=4;i++){
+    //     cout<<arr[i]<<endl;
+    // }
+    
+    
+//     int arr[5];
+//     cout<<"Enter the values of arrays: "<<endl;
+//     for(int i=0;i<=4;i++){
+//         cin>>arr[i];
+//     }
+//     for(int i=0;i<=4;i++){
+//         cout<<arr[i]<<" ";
+//     }
+
+
+//   int arr[5];
+//   cout<<"Enter the values arrays:"<<endl;
+// for(int i=0;i<=4;i++)
+// {
+//     cin>>arr[i];
+// }
+// for(int i=0;i<=4;i++)
+// {
+//     cout<<arr[i]<<" ";
+// }
+// arr[0]=100;// update value
+// cout<<endl;
+//    for(int i=0;i<=4;i++)
+// {
+//     cout<<arr[i]<<" ";
+// }
+
+// int arr[6]={1,2,3,4,5,6};
+// // output reverse
+// for(int i=5;i>=0;i--){
+//     cout<<arr[i]<<" ";
+// }
+// int X=3;
+// cout<<X<<endl;
+// change(X);
+// cout<<X<<endl;
+//  int size=10,b[size];
+// passing array to functions
+// int arr[3]={10,20,30};
+// for(int i=0;i<=2;i++)
+// {
+//     cout<<arr[i]<<" ";
+// }
+// cout<<endl;
+// arr[0]= 40;
+// change(arr);
+// for(int i=0;i<=2;i++)
+// {
+//     cout<<arr[i]<<" ";
+// }
+// 2- sum of arrays
+// int arr[5]={10,30,40,50,30};
+// int sum=0;
+// for(int i=0;i<=4;i++){
+//     sum+=arr[i];
+    
+// }
+// cout<<sum;
+
+// 3- size of array
+// int x;
+// cout<<sizeof(x);
+// cout<<endl;
+// double y;
+// cout<<sizeof(y);
+// cout<<endl;
+// min and max in array
+//  int arr[]={10,20,30,40,50,600,70,80};
+//  int n=sizeof(arr)/4;
+//  int mx=arr[0];
+//  for(int i=1;i<n;i++){
+//     // if(arr[i]>mx) mx=arr[i];
+//     mx=max(mx,arr[i]);
+//  }
+//  cout<<mx;
+
+//  int arr[]={10,-20,30,40,50,600,70,80};
+//  int n=sizeof(arr)/4;
+//  int mn=arr[0];
+//  for(int i=1;i<n;i++){
+//     if(arr[i]<mn) mn=arr[i];
+    
+//    // mn=min(mn,arr[i]);
+//  }
+//  cout<<mn;
+// cout<<INT_MAX;
+//   int arr[]={10,-20,30,40,50,600,70,80};
+//   int n=sizeof(arr)/4;
+//   int mx=INT_MIN;
+//   for(int i=1;i<n;i++){
+//     mx=max(mx,arr[i]);
+    
+//   }
+//   cout<<mx;
+//   cout<<endl;
+//    int arr[]={10,-20,30,40,50,600,70,80};
+//   int n=sizeof(arr)/4;
+//   int mn=INT_MAX;
+//   int mx=INT_MIN;
+//   for(int i=1;i<n;i++){
+//     mn=min(mn,arr[i]);
+//     mx=max(mx,arr[i]);
+    
+//   }
+//   cout<<mn;
+//   cout<<endl;
+//   cout<<mx;
+// int a[]={1,2,3,4,5,6,7,8};
+// int n=sizeof(a)/4;
+// int b[n];
+// for(int i=0;i<n;i++){
+//     // int j=n-1-i;
+//     // b[i]=a[j];
+//     b[i]=a[n-1-i];
+//     cout<<b[i]<<" ";
+// }
+// for(int i=0;i<n;i++){
+//     cout<<b[i]<<" ";
+// }
+// program to reverse the array without using extra array
+// hint using swapping
+//   int a[]={10,20,30,40,50,60,70,80};
+//   int n=sizeof(a)/4;
+//   for(int i=0;i<n;i++){
+//     cout<<a[i]<<" ";
+
+//   }
+//   int i=0;
+//   int j=n-1;
+//   while(i<j){
+//     int temp=a[i];
+//     a[i]=a[j];
+//     a[j]=temp;
+//     i++;
+//     j--;
+
+//   }
+//   cout<<endl;
+//     for(int i=0;i<n;i++){
+//     cout<<a[i]<<" ";
+
+//   }
+
+ 
+
+}
+
+
+
+                  
+
+    
+
+    
+    
+
